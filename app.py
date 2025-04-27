167b7575bb6ed7f6493bb6c15420711ad3cbdf86
NghiaDuong24904 <trungnghia.24904@gmail.com>
2025-04-27 19:34:33 +0700

    update

--- app.py
+++ app.py
@@ -1,25 +1,16 @@
 from flask import Flask, request, jsonify
 from PIL import Image
 import torch
-from torchvision import models, transforms
+from torchvision import transforms
 from huggingface_hub import hf_hub_download
 from model_arch import CatDogClassifier
 
 app = Flask(__name__)
 
-# Load pre-trained model (example with ResNet18)
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-repo_id = "Gumangusi/CatDogClassifier"  # Thay bằng repo của bạn
-filename = "catdog_final.pth"
-model_path = hf_hub_download(repo_id=repo_id, filename=filename)
 
-# Load mô hình
-model = CatDogClassifier(backbone_name='convnextv2_base.fcmae_ft_in22k_in1k', pretrained_backbone=False)
-model.load_state_dict(torch.load(model_path, map_location=device, weights_only=True))
-model = model.to(device)
-model.eval()  # Đặt mô hình ở chế độ đánh giá
-logger.info("Model loaded successfully")
-
+# Lazy loading model
+model = None
 transform = transforms.Compose([
     transforms.Resize(256),
     transforms.CenterCrop(224),
@@ -27,16 +18,32 @@ transform = transforms.Compose([
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
+def load_model():
+    global model
+    if model is None:
+        repo_id = "Gumangusi/CatDogClassifier"
+        filename = "catdog_final.pth"
+        model_path = hf_hub_download(repo_id=repo_id, filename=filename)
+
+        model = CatDogClassifier(backbone_name='convnextv2_base.fcmae_ft_in22k_in1k', pretrained_backbone=False)
+        model.load_state_dict(torch.load(model_path, map_location=device), strict=False)
+        model = model.to(device)
+        model.eval()
+
 @app.route('/predict', methods=['POST'])
 def predict():
+    load_model()
+
     img_file = request.files['image']
-    img = Image.open(img_file.stream)
-    img = transform(img).unsqueeze(0)
+    img = Image.open(img_file.stream).convert('RGB')
+    img = transform(img).unsqueeze(0).to(device)
+
     with torch.no_grad():
         output = model(img)
         pred = torch.sigmoid(output).item()
         label = "Dog" if pred > 0.5 else "Cat"
+
     return jsonify({'label': label})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
