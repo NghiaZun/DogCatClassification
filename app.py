19b0b41e938f42af1a5a42806181b4064973f644
NghiaDuong24904 <trungnghia.24904@gmail.com>
2025-04-27 22:47:56 +0700

    end

--- app.py
+++ app.py
@@ -111,6 +111,3 @@ def predict():
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=10000, debug=False)
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=10000, debug=False)
