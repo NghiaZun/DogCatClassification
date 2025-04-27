563cf708a0a1cc8b7c68680f38e847f918fd4116
NghiaDuong24904 <trungnghia.24904@gmail.com>
2025-04-28 04:01:57 +0700

    fix app

--- app.py
+++ app.py
@@ -18,19 +18,14 @@ transform = transforms.Compose([
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
-def load_model():
-    global model
-    if model is None:
-        repo_id = "Gumangusi/CatDogClassifier"
-        filename = "catdog_final.pth"
-        model_path = hf_hub_download(repo_id=repo_id, filename=filename)
+repo_id = "Gumangusi/CatDogClassifier"
+filename = "catdog_final.pth"
+model_path = hf_hub_download(repo_id=repo_id, filename=filename)
 
-        model = CatDogClassifier(backbone_name='convnextv2_base.fcmae_ft_in22k_in1k', pretrained_backbone=False)
-        model.load_state_dict(torch.load(model_path, map_location=device), strict=False)
-        model = model.to(device)
-        model.eval()
-
-load_model()
+model = CatDogClassifier(backbone_name='convnextv2_base.fcmae_ft_in22k_in1k', pretrained_backbone=False)
+model.load_state_dict(torch.load(model_path, map_location=device), strict=False)
+model = model.to(device)
+model.eval()
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -73,23 +68,18 @@ transform = transforms.Compose([
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
-def load_model():
-    global model
-    if model is None:
-        print("Loading model...")  # Log khi mô hình bắt đầu tải
-        repo_id = "Gumangusi/CatDogClassifier"
-        filename = "catdog_final.pth"
-        model_path = hf_hub_download(repo_id=repo_id, filename=filename)
+print("Loading model...")  # Log khi mô hình bắt đầu tải
+repo_id = "Gumangusi/CatDogClassifier"
+filename = "catdog_final.pth"
+model_path = hf_hub_download(repo_id=repo_id, filename=filename)
 
-        model = CatDogClassifier(backbone_name='convnextv2_base.fcmae_ft_in22k_in1k', pretrained_backbone=False)
-        model.load_state_dict(torch.load(model_path, map_location=device), strict=False)
-        model = model.to(device)
-        model.eval()
+model = CatDogClassifier(backbone_name='convnextv2_base.fcmae_ft_in22k_in1k', pretrained_backbone=False)
+model.load_state_dict(torch.load(model_path, map_location=device), strict=False)
+model = model.to(device)
+model.eval()
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    load_model()
-
     img_file = request.files['image']
     print("Received image:", img_file.filename)  # Log để kiểm tra
 
